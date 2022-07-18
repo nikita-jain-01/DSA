ca10e36f38bd12c1d79800d19a4ae9514270983d
Nikita Jain <nikita0730.cse19@chitkara.edu.in>
2022-07-19 00:47:50 +0530

    Both trees are same or not

---
+++ BinaryTrees/Same_tree.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null)
+            return true;
+        else if((p==null && q!=null) || (q==null && p!=null) || (p.val!=q.val))
+            return false;
+        else
+            return (isSameTree(p.left,q.left) && isSameTree(p.right,q.right));
+    }
+}
